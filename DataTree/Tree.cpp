a37ee089925fda0b69306da77d4db08c8e6ed0c7
Liam Kikuchi <liamshinx@gmail.com>
2019-12-03 08:59:33 -0800

    Add project files.

---
+++ DataTree/Tree.cpp
@@ -0,0 +1,17 @@
+#include "Tree.h"
+
+Tree::Tree() {
+
+}
+
+bool Tree::insert(int newVal) {
+
+}
+
+void Tree::print(ostream& ostr, Node* curNode) const {
+
+}
+
+ostream& operator<<(ostream& ostrm, const Tree& tree) {
+
+}
